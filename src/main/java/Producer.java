6526fa28baa86d116ebbea67bd7e120571aec7ef
slavik <slavayus@gmail.com>
2019-01-26 20:31:05 +0300

    created direct exchange

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,22 +1,21 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.MessageProperties;
 
 import java.nio.charset.StandardCharsets;
 
 public class Producer {
-    private final static String QUEUE_NAME = "task_queue";
+    private final static String EXCHANGE_NAME = "direct_users";
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
 
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME, "adult", null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
